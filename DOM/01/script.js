5a7a81f09f69078d6aebfefefce24e8ff5661b38
Dalibor <kovacic.dalibor@gmail.com>
2024-03-28 21:00:16 +0100

    DOM1

--- DOM/01/script.js
+++ DOM/01/script.js
@@ -3,3 +3,10 @@
 console.log(window);
 //document objekt , moze bez window.document
 console.log(document);
+
+console.log(document.body.innerHTML);
+
+console.log(document.body.innerText);
+
+//nije read only moze i ovako
+document.body.innerHTML = "<h1>Hello World</h1>";
