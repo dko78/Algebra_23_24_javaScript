274d7c5f7972dc7471e2a2f002146d3135c8f1b5
Dalibor <kovacic.dalibor@gmail.com>
2024-04-19 18:06:00 +0200

    od predavača 35 vjezba

---
+++ DOM/15-35-callbachHell/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function getData(putanja, cb) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", putanja);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      cb(JSON.parse(this.response));
+    }
+  };
+
+  setTimeout(() => {
+    xhr.send();
+  }, Math.floor(Math.random() * 2000) + 100);
+}
+
+getData("./filmovi.json", (filmovi) => {
+  console.log(filmovi);
+  getData("./glumci.json", (glumci) => {
+    console.log(glumci);
+    getData("./redatelji.json", (redatelji) => {
+      console.log(redatelji);
+    });
+  });
+});
