06827068a358bd0343fd8428f71b50d3e995788d
Dalibor Kovacic <32059793+dko78@users.noreply.github.com>
2024-04-24 09:46:00 +0200

    Update script.js

--- DOM/JS_PARCIJALNI_API/script.js
+++ DOM/JS_PARCIJALNI_API/script.js
@@ -1,43 +1,5 @@
 "use strict";
 
-//https://jsonplaceholder.typicode.com/users
-
-const urlTodo = "https://jsonplaceholder.typicode.com/users";
-
-const searchInput = document.querySelector("[data-search]");
-
-const userCardTemplate = document.querySelector("[data-user-template]");
-
-const userCardContainer = document.querySelector("[data-user-cards-container]");
-
-let users = [];
-
-const url =
-  "https://itunes.apple.com/search?term=jack+johnson&entity=musicVideo";
-
-//primjer web simpified
-fetch(urlTodo)
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((user) => {
-      const card = userCardTemplate.content.cloneNode(true).children[0];
-      const header = card.querySelector("[data-header]");
-      const body = card.querySelector("[data-body]");
-      header.textContent = user.name;
-      body.textContent = user.email;
-      console.log(card);
-      userCardContainer.append(card);
-    });
-    s;
-  })
-  .catch((error) => {
-    console.log(`ERROR: ${error}`);
-  });
-
-searchInput.addEventListener("input", (e) => {
-  const value = e.target.value;
-});
-/*
 function searchiTunes(term) {
   const url = `https://itunes.apple.com/search?term=${term}&media=music&entity=song`;
 
@@ -54,9 +16,9 @@ function searchiTunes(term) {
 
 searchiTunes("Taylor Swift");
 console.log("el radi");
-*/
+
 //2
-/*
+
 function searchiTunes2(term) {
   const url =
     "https://itunes.apple.com/search?term=jack+johnson&entity=musicVideo";
@@ -72,10 +34,3 @@ function searchiTunes2(term) {
       console.log(`ERROR: ${error}`);
     });
 }
-*/
-/*
-searchInput.addEventListener("input", (e) => {
-  const value = e.target.value;
-  console.log(value);
-});
-*/
